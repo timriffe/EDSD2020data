cb5afdb4d37337f97af279740b023e8cb8f8172a
Tim Riffe <tim.riffe@gmail.com>
2020-11-08 22:50:57 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # EDSD2020data
+
+Materials for the EDSD data wrangling module
+
+-Nov 9 17:00-20:00
+-Nov 10 17:00-20:00
+-Nov 11 15:00-18:00
+-Nov 12 15:00-18:00
+
+
---
+++ buildhtml.R
@@ -0,0 +1,12 @@
+library(rmarkdown)
+
+output_dir <- "../EDSD2020data/docs"
+
+
+render.this <- "../EDSD2020data/index.Rmd"
+render(render.this, output_dir = output_dir, 
+       params = list(output_dir = output_dir))
+
+render.this <- "../EDSD2020data/Day1.Rmd"
+render(render.this, output_dir = output_dir, 
+       params = list(output_dir = output_dir))
